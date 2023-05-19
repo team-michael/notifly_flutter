3d8068d4ed58ae15b80de59bc52a44bd95468ac8
minyonglee <rickylee87@gmail.com>
2023-05-19 18:06:53 +0900

    Firebase setup for example app with flutterfire

--- notifly_flutter/example/lib/main.dart
+++ notifly_flutter/example/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:notifly_flutter/notifly_flutter.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await NotiflyPlugin.initialize(
     dotenv.env['NOTIFLY_PROJECT_ID']!,
